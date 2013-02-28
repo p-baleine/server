6e650e642024034771cd51b0f6714c1c44f7fe92
Tajima Junpei <p.baleine@gmail.com>
2013-03-01 06:36:17 +0900

    remove basic auth

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.basicAuth('tobi', 'ferret'));
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/repos'));
 
